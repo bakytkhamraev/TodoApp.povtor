3dc8682554a8a2e2c847b5992e42d01cd93bf4ad
KunduzNbkva <kunduznbkva@gmail.com>
2020-02-27 18:42:00 +0600

    Add RadioGroup without logic,sending data to "note.txt"

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.todoapp;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -7,6 +8,9 @@ import com.example.todoapp.ui.onBoard.OnBoardActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.os.Environment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,10 +30,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.EditText;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private final int RC_WRITE_EXTERNAL=101;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +76,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        initFile("");
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -81,6 +99,8 @@ public class MainActivity extends AppCompatActivity {
             case (R.id.action_settings):
                 Prefs.getInstance(this).delete();
                 startActivity(new Intent(this, OnBoardActivity.class));
+            case (R.id.action_size) :
+                startActivity(new Intent(MainActivity.this,SizeActivity.class));
         break;
         }
 
@@ -100,4 +120,34 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode==RESULT_OK&&resultCode==100){
        String title= data.getStringExtra("title");
     }
-}}
+}
+    @AfterPermissionGranted(RC_WRITE_EXTERNAL)
+    public void initFile(String notes){
+        String permission= Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        if(EasyPermissions.hasPermissions(this,permission)) {
+            File folder = new File(Environment.getExternalStorageDirectory(), "ToDoApp6");
+            folder.mkdirs();
+            File file=new File(folder,"note.txt");
+            try {
+                file.createNewFile();
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                fileOutputStream.write(notes.getBytes());
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else{
+            EasyPermissions.requestPermissions(this,"Разреши!",RC_WRITE_EXTERNAL,permission);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        editText=findViewById(R.id.zametki_edit_text);
+        initFile(editText.getText().toString());
+        super.onBackPressed();
+    }
+
+
+
+}
--- app/src/main/java/com/example/todoapp/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/todoapp/ui/slideshow/SlideshowFragment.java
@@ -1,9 +1,12 @@
 package com.example.todoapp.ui.slideshow;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,24 +15,19 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.todoapp.MainActivity;
 import com.example.todoapp.R;
 
 public class SlideshowFragment extends Fragment {
 
     private SlideshowViewModel slideshowViewModel;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
+
+
 }
